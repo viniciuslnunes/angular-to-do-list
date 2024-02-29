e4be4303373a633c6e10ff47c6ed676cf9c6e89d
viniciuslnunes <viniciuslopes_nunes@hotmail.com>
2024-02-29 19:34:04 -0300

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+//Pages
+import { ListComponent } from './modules/to-do-list/pages/list/list.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: ListComponent
+    }
+];
