10aa64e04fc344cdcda7ab314451f9f927587183
viniciuslnunes <viniciuslopes_nunes@hotmail.com>
2024-03-04 18:07:00 -0300

    to-do-list components made

---
+++ src/app/modules/to-do-list/components/input-list-item/input-list-item.component.ts
@@ -0,0 +1,22 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { IListItems } from '../../interface/IListItems.inteface';
+
+@Component({
+  selector: 'app-input-list-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './input-list-item.component.html',
+  styleUrl: './input-list-item.component.scss'
+})
+export class InputListItemComponent {
+  @Input({required: true}) public inputListItems: IListItems[] = []; 
+  
+  @Output() public outputUpdateItemCheckbox = new EventEmitter<{
+    id: string;
+    checked: boolean;
+  }>;
+
+  public updateItemCheckbox(id: string, checked: boolean) {
+    return this.outputUpdateItemCheckbox.emit({id, checked});
+  }
+}
--- src/app/modules/to-do-list/pages/list/list.component.ts
+++ src/app/modules/to-do-list/pages/list/list.component.ts
@@ -1,13 +1,64 @@
 import { Component, signal } from '@angular/core';
 import { InputAddItemComponent } from '../../components/input-add-item/input-add-item.component';
+import { IListItems } from '../../interface/IListItems.inteface';
+import { InputListItemComponent } from '../../components/input-list-item/input-list-item.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [InputAddItemComponent],
+  imports: [InputAddItemComponent, InputListItemComponent],
   templateUrl: './list.component.html',
-  styleUrl: './list.component.css'
+  styleUrl: './list.component.scss',
 })
 export class ListComponent {
   public addItem = signal(true);
+
+  #setListItems = signal<IListItems[]>(this.#parseItems());
+  public getListItems = this.#setListItems.asReadonly();
+
+  #parseItems() {
+    return JSON.parse(localStorage.getItem('@my-list') || '[]');
+  }
+
+  public getInputAndAddItem(value: IListItems) {
+    localStorage.setItem(
+      '@my-list',
+      JSON.stringify([...this.#setListItems(), value])
+    );
+
+    return this.#setListItems.set(this.#parseItems());
+  }
+
+  public listItemsStage(value: 'pending' | 'completed') {
+    return this.getListItems().filter((res: IListItems) => {
+      if (value == 'pending') {
+        return !res.checked;
+      }
+
+      if (value == 'completed') {
+        return res.checked;
+      }
+
+      return res;
+    });
+  }
+
+  public deleteAllItems() {
+    localStorage.removeItem('@my-list');
+    return this.#setListItems.set(this.#parseItems());
+  }
+
+  public updateItemCheckbox(newItem: { id: string; checked: boolean }) {
+    this.#setListItems.update((oldValue: IListItems[]) => {
+      oldValue.filter((res) => {
+        if (res.id === newItem.id) {
+          res.checked = newItem.checked;
+          return res;
+        }
+
+        return res;
+      });
+      return oldValue;
+    });
+  }
 }
